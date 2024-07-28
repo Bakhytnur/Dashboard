efbd5c577e0ba0eefbf4e99efcc7db10204afe87
Bakhytnur <b.s.gabdullin@gmail.com>
2024-07-28 16:56:38 +0500

    commit add rightbar footer

---
+++ src/app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.logo}>gabdullin</div>
+        <div className={styles.text}>All rights reserved</div>
+    </div>
+  )
+}
+
+export default Footer
