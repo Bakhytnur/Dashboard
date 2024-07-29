d443bd1670725f0f3fafc59402bb4867633e18b3
Bakhytnur <b.s.gabdullin@gmail.com>
2024-07-29 18:01:03 +0500

    commit add users products pagination

--- src/app/dashboard/products/page.jsx
+++ src/app/dashboard/products/page.jsx
@@ -4,8 +4,16 @@ import styles from '@/app/ui/dashboard/products/products.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import { fetchProducts } from '@/app/lib/data'
 
-const ProductsPage = () => {
+const ProductsPage = async ({searchParams}) => {
+  const q = searchParams?.q || "";
+  const page = searchParams?.page || 1;
+  const {count, products} = await fetchProducts(q, page);
+
+  console.log('searchParams', searchParams);
+  console.log('q', q);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -26,29 +34,31 @@ const ProductsPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {products.map(product => (
+          <tr key={product.id}>
             <td>
               <div className={styles.product}>
-                <Image src="/noproduct.jpg" alt="" width={40} height={40} className={styles.productImage} />
-                IPhone
+                <Image src={product.img || "/noproduct.jpg"} alt="" width={40} height={40} className={styles.productImage} />
+                {product.title}
               </div>
             </td>
-            <td>Desc</td>
-            <td>$999</td>
-            <td>15.05.2023</td>
-            <td>72</td>
+            <td>{product.desc}</td>
+            <td>{product.price}</td>
+            <td>{product.createdAt?.toString().splice(4,16)}</td>
+            <td>{product.stock}</td>
             <td>
               <div className={styles.buttons}>
-                <Link href='/dashboard/products/test'>
+                <Link href={`/dashboard/products/${product.id}`}>
                   <button className={`${styles.button} ${styles.view}`}>View</button>
                 </Link>
                   <button className={`${styles.button} ${styles.delete}`}>Delete</button>
               </div>
             </td>
           </tr>
+          ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   )
 }
